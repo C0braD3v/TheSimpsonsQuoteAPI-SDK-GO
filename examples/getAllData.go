127ae47c3984c58f8be1f4aebed6d3361551799c
C:\0BRA\D3V <hpaulson@smc.wtf>
2020-02-22 13:04:13 -0500

    Update getAllData.go

--- examples/getAllData.go
+++ examples/getAllData.go
@@ -13,3 +13,5 @@ func main() {
 	}
 	fmt.Println(data[0])
 }
+
+// Prints all data from the random quote. i.e: {Facts are meaningless. You could use facts to prove anything that's even remotely true.  https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939 }
